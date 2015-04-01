946a674fa9170db0f0f14e167d6566a58de43407
Jason Dreyzehner <jason@dreyzehner.com>
2015-03-31 21:43:04 -0400

    gulp, lint and cover

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+var bin = 'bin/*';
+var js = '*.js';
+var lib = 'lib/**/*.js';
+var tests = 'test/**/*.{js, json}';
+var developing = false;
+
+gulp.task('default', function(){
+  developing = true;
+  runSequence(['watch']);
+});
+
+gulp.task('watch', ['jshint', 'test'], function(){
+  gulp.watch([bin, js, lib, tests], ['jshint', 'test']);
+});
+
+gulp.task('test', ['rm-coverage'], function(cb) {
+  gulp.src([bin, lib])
+    .pipe($.istanbul())
+    .pipe($.istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(['test/*.js'], {read: false})
+        .pipe($.mocha())
+        .pipe($.istanbul.writeReports())
+        .on('end', cb);
+    });
+});
+
+gulp.task('jshint', function () {
+  return gulp.src([bin, js, lib, tests])
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!developing, $.jshint.reporter('fail')));
+});
+
+gulp.task('coveralls', function () {
+  return gulp.src('coverage/lcov.info')
+    .pipe($.coveralls());
+});
+
+gulp.task('rm-coverage', function (cb) {
+    (require('rimraf'))('coverage', cb);
+});
--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
-var assert = require("assert");
+'use strict';
+
+var assert = require('assert');
+var s18n = require('../');
 
 describe('s18n', function(){
     it('should have a test', function(){
-      assert.ok(true);
-    })
+      assert.ok(s18n());
+    });
 });
