6ee1763f1b3e5c06a159d04c1a2ddb357a894fe1
Jason Dreyzehner <jason@dreyzehner.com>
2015-03-31 23:03:10 -0400

    fix coverage watch

--- gulpfile.js
+++ gulpfile.js
@@ -18,28 +18,15 @@ gulp.task('watch', ['test'], function(){
   gulp.watch([bin, js, lib, tests], ['test']);
 });
 
-gulp.task('test', ['jshint','rm-coverage'], function(cb) {
-  gulp.src([bin, lib])
-    .pipe($.istanbul())
-    .pipe($.istanbul.hookRequire())
-    .on('finish', function () {
-      gulp.src(['test/*.js'], {read: false})
-        .pipe($.mocha())
-        .pipe($.istanbul.writeReports())
-        .on('end', cb);
-    });
-});
+gulp.task('test', ['jshint', 'rm-coverage'], $.shell.task([
+    './node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha'
+]));
 
 gulp.task('jshint', function () {
   return gulp.src([bin, js, lib, tests])
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.if(!developing, $.jshint.reporter('fail')));
-});
-
-gulp.task('coveralls', function () {
-  return gulp.src('coverage/lcov.info')
-    .pipe($.coveralls());
 });
 
 gulp.task('rm-coverage', function (cb) {
