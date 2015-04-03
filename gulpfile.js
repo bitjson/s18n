6872d8d0dd0a9e62cac33bfcf30abe37bc920a09
Jason Dreyzehner <jason@dreyzehner.com>
2015-04-02 21:05:59 -0500

    scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,23 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
+var run = require('run-sequence');
 var bin = 'bin/*';
 var js = '*.js';
 var lib = 'lib/**/*.js';
 var tests = 'test/**/*.{js, json}';
 var developing = false;
 
-gulp.task('default', function(){
-  developing = true;
-  runSequence(['watch']);
-});
+gulp.task('default', ['watch']);
 
-gulp.task('watch', ['test'], function(){
+gulp.task('watch', function(){
+  developing = true;
+  run('test');
   gulp.watch([bin, js, lib, tests], ['test']);
 });
 
 gulp.task('test', ['jshint', 'rm-coverage'], $.shell.task([
-    './node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha'
+  './node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --print detail'
 ]));
 
 gulp.task('jshint', function () {
--- test/test.js
+++ test/test.js
@@ -3,8 +3,32 @@
 var assert = require('assert');
 var s18n = require('../');
 
-describe('s18n', function(){
-    it('should have a test', function(){
-      assert.ok(s18n());
+describe('s18n', function() {
+
+  describe('.extract()', function() {
+
+    it('should have an "extract" method', function() {
+      assert.equal(typeof s18n, 'object');
+      assert.equal(typeof s18n.extract, 'function');
     });
+
+  });
+
+  describe('.localize()', function() {
+
+    it('should have a "localize" method', function() {
+      assert.equal(typeof s18n, 'object');
+      assert.equal(typeof s18n.extract, 'function');
+    });
+
+  });
+
+  describe('.map()', function() {
+
+    it('should have a "map" method', function() {
+      assert.equal(typeof s18n, 'object');
+      assert.equal(typeof s18n.extract, 'function');
+    });
+
+  });
 });
