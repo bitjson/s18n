73fbcef06ff0b387c69b645b1fcc8d0d6dbf7e20
Jason Dreyzehner <jason@dreyzehner.com>
2015-05-24 17:55:17 -0400

    compareLocales throws error if either locale is not an object

--- test/spec.compareLocales.js
+++ test/spec.compareLocales.js
@@ -10,6 +10,21 @@ describe('s18n.compareLocales()', function() {
     assert.equal(typeof s18n.compareLocales, 'function');
   });
 
+  it('should error if either locale is not an object', function() {
+    var localeA = 'a string';
+    var localeB = {
+      '37b51d19': 'bar',
+      'acbd18db': 'fóó',
+      '149603e6': 'óld'
+    };
+    assert.throws(
+      function() {
+        s18n.compareLocales(localeA, localeB);
+      },
+      /`localeA`/,
+      'unexpected error message');
+  });
+
   it('should return a locale comparison array', function() {
     var localeA = {
       '37b51d19': 'bar',
@@ -24,36 +39,28 @@ describe('s18n.compareLocales()', function() {
     var comparisonArray = s18n.compareLocales(localeA, localeB);
     assert.deepEqual(comparisonArray, [
       //0: hashes missing from localeA (present in localeB)
-      [
-        {
-          hash: '149603e6',
-          string: 'óld'
-        }
-      ],
+      [{
+        hash: '149603e6',
+        string: 'óld'
+      }],
       //1: hashes missing from localeB (present in localeA)
-      [
-        {
-          hash: '73feffa4',
-          string: 'baz'
-        }
-      ],
+      [{
+        hash: '73feffa4',
+        string: 'baz'
+      }],
       //2: hashes with duplicate strings (likely untranslated)
-      [
-        {
-          hash: '37b51d19',
-          string: 'bar'
-        }
-      ],
+      [{
+        hash: '37b51d19',
+        string: 'bar'
+      }],
       //3: hashes with modified strings
-      [
-        {
-          hash: 'acbd18db',
-          strings: [
-            'foo',
-            'fóó'
-          ]
-        }
-      ]
+      [{
+        hash: 'acbd18db',
+        strings: [
+          'foo',
+          'fóó'
+        ]
+      }]
     ]);
   });
 
